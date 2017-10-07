f747c5846c2a7d11760f9c92428d10cf932cadda
alex.zhdanov <webmaster@webskysoft.ru>
2017-10-07 09:22:52 +0700

    Способы поиска нужного HTML-элемента final

---
+++ html-element-collection/audioplayer/js/common.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const player = document.getElementsByClassName('mediaplayer')[0];
+const audio = document.getElementsByTagName('audio')[0];
+const buttonPlayPause = document.getElementsByClassName('playstate')[0];
+const title = document.getElementsByClassName('title')[0];
+const buttonStop = document.getElementsByClassName('stop')[0];
+const buttonNext = document.getElementsByClassName('next')[0];
+const buttonBack = document.getElementsByClassName('back')[0];
+const tracks = ['mp3/LA Chill Tour.mp3', 'mp3/LA Fusion Jam.mp3', 'mp3/This is it band.mp3'];
+const tracksTitle = ['LA Chill Tour', 'LA Fusion Jam', 'This is it band'];
+
+let item = 0;
+
+buttonPlayPause.onclick = function () {
+	player.classList.toggle('play');
+	player.classList.contains('play') ? audio.play() :	audio.pause();
+}
+
+buttonStop.onclick = function () {
+	player.classList.remove('play');
+	audio.pause();
+	audio.currentTime = 0;
+}
+
+buttonNext.onclick = function () {
+	player.classList.remove('play');
+	if (item < tracks.length - 1) {
+		item++;
+		title.title = tracksTitle[item];
+		audio.src = tracks[item];
+  }
+  else {
+		item = 0;
+		title.title = tracksTitle[item];
+		audio.src = tracks[item];
+  }
+}
+
+buttonBack.onclick = function () {
+	player.classList.remove('play');
+	if (item > 0) {
+  	item--;
+		title.title = tracksTitle[item];
+		audio.src = tracks[item];
+	}
+	else {
+    item = tracks.length-1;
+    title.title = tracksTitle[item];
+		audio.src = tracks[item];
+  }
+}
