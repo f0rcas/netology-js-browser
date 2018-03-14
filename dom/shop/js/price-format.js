57c9c986e3bee1821b61ba5c76f9eb0e82e555e2
alex.zhdanov <webmaster@webskysoft.ru>
2018-03-14 20:05:24 +0700

    finish

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,18 @@
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+const addProduct = document.querySelectorAll('.add');
+const productsPrices = document.querySelectorAll('p');
+const cartCount = document.querySelector('#cart-count');
+const totalPrice = document.querySelector('#cart-total-price');
+
+function fillBox() {
+  let zeroPrice  = totalPrice.innerHTML;
+  totalPrice.innerHTML = +zeroPrice + +this.dataset.price;
+  cartCount.innerHTML ++;
+}
+
+Array.from(addProduct).forEach(product => {
+  product.addEventListener('click', fillBox);
+});
