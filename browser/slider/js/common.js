95a7c872635e35fca82a7c4d3d2ae4faa81a4346
alex.zhdanov <webmaster@webskysoft.ru>
2018-03-17 17:31:19 +0700

    общий код: img.src = images[item]; вынес за пределы if

--- browser/slider/js/common.js
+++ browser/slider/js/common.js
@@ -4,14 +4,12 @@ const img = document.getElementById('slider');
 const images = ['./i/airmax.png', './i/airmax-jump.png', './i/airmax-on-foot.png', './i/airmax-playground.png', './i/airmax-top-view.png'];
 
 let item = 0;
-img.src = images[item];
 setInterval(() => {
+  img.src = images[item];
 	if (item < images.length - 1) {
     item++;
-    img.src = images[item];
   }
   else {
     item = 0;
-    img.src = images[item];
   }
 }, 5000);
